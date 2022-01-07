c91dbdb8547cab6de0527e75d5f0640841ba046a
이정윤 <dlwjddbs14@naver.com>
2022-01-08 01:08:27 +0900

    ⚡ :: user settings

--- CL-INSIDE/src/main/java/com/example/sns/global/security/auth/CustomUserDetailService.java
+++ CL-INSIDE/src/main/java/com/example/sns/global/security/auth/CustomUserDetailService.java
@@ -15,7 +15,6 @@ public class CustomUserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-
         return userRepository.findByEmail(username)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
