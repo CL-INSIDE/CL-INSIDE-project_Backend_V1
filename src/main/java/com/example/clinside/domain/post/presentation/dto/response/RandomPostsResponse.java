55fdcf16a69e5c4eae2a708ee6445cebc3c9dd26
이정윤 <dlwjddbs14@naver.com>
2022-01-10 03:12:35 +0900

    ⚡ :: random get 3 posts settings

--- src/main/java/com/example/clinside/domain/post/presentation/dto/response/RandomPostsResponse.java
+++ src/main/java/com/example/clinside/domain/post/presentation/dto/response/RandomPostsResponse.java
@@ -1,2 +1,18 @@
-package com.example.clinside.domain.post.presentation.dto.response;public class RandomPostsResponse {
+package com.example.clinside.domain.post.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class RandomPostsResponse {
+
+    private Integer userId;
+    private Integer postId;
+
+    private String title;
+    private Integer likeCounts;
+    private String image;
 }
