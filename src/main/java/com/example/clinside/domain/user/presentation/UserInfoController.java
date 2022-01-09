f640851d92d069246c4f17920194a3deac4fc63c
이정윤 <dlwjddbs14@naver.com>
2022-01-09 23:15:53 +0900

    ⚡ :: settings userlike posts

--- src/main/java/com/example/clinside/domain/user/presentation/UserInfoController.java
+++ src/main/java/com/example/clinside/domain/user/presentation/UserInfoController.java
@@ -1,5 +1,7 @@
 package com.example.clinside.domain.user.presentation;
 
+import com.example.clinside.domain.post.presentation.dto.response.PostResponse;
+import com.example.clinside.domain.user.service.GetUserLikePosts;
 import com.example.clinside.domain.user.presentation.dto.response.UserProfilePostResponse;
 import com.example.clinside.domain.user.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
@@ -7,14 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class UserInfoController {
 
+    private final GetUserLikePosts getUserLikePosts;
     private final UserProfileService userProfileService;
 
     @GetMapping("/info/{user-id}")
     public UserProfilePostResponse getUserProfile(@PathVariable(name = "user-id") Integer id) {
         return userProfileService.execute(id);
+    }
+
+    @GetMapping("/like")
+    public List<PostResponse> getLikePosts(){
+        return getUserLikePosts.execute();
     }
 }
--- src/main/java/com/example/clinside/domain/user/service/GetUserLikePosts.java
+++ src/main/java/com/example/clinside/domain/user/service/GetUserLikePosts.java
@@ -1,8 +1,11 @@
-package com.example.clinside.domain.post.service;
+package com.example.clinside.domain.user.service;
 
 import com.example.clinside.domain.auth.facade.UserFacade;
 import com.example.clinside.domain.emotion.domain.repository.LikeRepository;
+import com.example.clinside.domain.image.domain.Image;
+import com.example.clinside.domain.image.domain.repository.ImageRepository;
 import com.example.clinside.domain.post.presentation.dto.response.PostResponse;
+import com.example.clinside.global.exception.ImageNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,7 @@ import java.util.stream.Collectors;
 public class GetUserLikePosts {
 
     private final LikeRepository likeRepository;
+    private final ImageRepository imageRepository;
 
     public List<PostResponse> execute() {
         return likeRepository.findByUserId(UserFacade.getUserId())
@@ -21,9 +25,13 @@ public class GetUserLikePosts {
                 .map(like -> {
                     PostResponse response = PostResponse.builder()
                             .id(like.getPost().getId())
+                            .userId(like.getUser().getId())
                             .title(like.getPost().getTitle())
                             .content(like.getPost().getContent())
                             .isLike(true)
+                            .image(imageRepository.findByPostId(like.getPost().getId())
+                                    .map(Image::getImageUrl)
+                                    .orElseThrow(() -> ImageNotFoundException.EXCEPTION))
                             .build();
                     return response;
                 })
