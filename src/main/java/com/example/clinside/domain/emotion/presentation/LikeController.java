c4698faf026e6d62428a749803a5613557c58353
이정윤 <dlwjddbs14@naver.com>
2022-01-13 01:51:40 +0900

    ⚡ :: like settings

--- src/main/java/com/example/clinside/domain/emotion/presentation/LikeController.java
+++ src/main/java/com/example/clinside/domain/emotion/presentation/LikeController.java
@@ -1,24 +1,26 @@
 package com.example.clinside.domain.emotion.presentation;
 
-import com.example.clinside.domain.emotion.service.AddLikePostService;
-import com.example.clinside.domain.emotion.service.RemoveLikePostService;
+import com.example.clinside.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/like")
 public class LikeController {
 
-    private final AddLikePostService addLikePostService;
-    private final RemoveLikePostService removeLikePostService;
+    private final PostService postService;
 
-    @PostMapping("/like/{post-id}")
+    @PostMapping("/{post-id}")
+    @ResponseStatus(HttpStatus.CREATED)
     public void addLike(@PathVariable(name = "post-id") Integer id) {
-        addLikePostService.execute(id);
+        postService.addLike(id);
     }
 
-    @DeleteMapping("/like/{post-id}")
+    @DeleteMapping("/{post-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeLike(@PathVariable(name = "post-id") Integer id) {
-        removeLikePostService.execute(id);
+        postService.removeLike(id);
     }
 }
